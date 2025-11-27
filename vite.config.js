d0164725decd769eaaa4c494bcdcf3bb748d6ad1
Arnav Kant <kantarnav2@gmail.com>
2025-11-27 23:27:16 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const PORT = process.env.PORT || 5173;
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: PORT || 5173,
+        port: PORT,
     },
 })
